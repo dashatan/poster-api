0866b5fb9425a7ac991d21fe0d0a6461f01507a0
abbas <dashatanad@gmail.com>
2022-11-15 03:05:44 +0330

    redis added

--- routes/resolvers/tmpImageUpload.js
+++ routes/resolvers/tmpImageUpload.js
@@ -1,12 +1,25 @@
 const Post = require("../../graphql/models/Post");
+const { redisClient } = require("../../server");
 
 const tmpImageUpload = async (req, res) => {
-    try {
-        const file = req.file;
-        res.json(file);
-    } catch (error) {
-        res.status(500).json(error.message);
-    }
+  try {
+    const file = req.file;
+    const userId = req.body.userId;
+    const key = userId + "_TempPostImages";
+    redisClient
+      .get(key)
+      .then((val) => {
+        console.log(val);
+      })
+      .catch(() => {
+        console.log(file);
+        redisClient.set(key,[file.filename])
+      });
+
+    res.json(file);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
 
-module.exports = tmpImageUpload
+module.exports = tmpImageUpload;
